161419262d515dbeaa162ff0aa9ab59cd4f26d6e
ss35789 <ss35789@naver.com>
2022-07-06 02:32:54 +0900

    Update funtion of btn

--- clone3/src/App.js
+++ clone3/src/App.js
@@ -2,12 +2,22 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
+import { useState } from 'react';
+import Main from'./components/main';
 function App() {
+  const [toptext,setToptext]=useState("Top text")
+  const [bottomtext,setBottomtext]=useState("Bottom text")
+
   return (
     <div className="App">
       <Header></Header>
       <body>
-        <Meme></Meme>
+        <Meme makeIt={(Toptext,Bottomtext)=>{
+          const newToptext={Toptext:Toptext}
+          setToptext(newToptext.Toptext)
+          setBottomtext(Bottomtext)
+        }}></Meme>
+        <Main toptext={toptext} bottomtext={bottomtext}></Main>
       </body>
       
     </div>
--- clone3/src/components/Meme.js
+++ clone3/src/components/Meme.js
@@ -1,20 +1,29 @@
 import React from 'react'
 
-export default function Meme(){
+export default function Meme(props){
     return(
         <main>
-            <form className='form'>
+            <form className='form' onSubmit={event=>{
+                event.preventDefault();
+                const Toptext=event.target.Toptext.value
+                const Bottomtext=event.target.Bottomtext.value
+            
+                props.makeIt(Toptext,Bottomtext);
+            }}>
                 <input 
                     type ='text' 
                     className='form--input'
                     placeholder='Top text'
+                    name='Toptext'
+                    
                     ></input>
                 <input 
                     type ='text' 
                     className='form--input'
                     placeholder='Bottom text'
+                    name='Bottomtext'
                     ></input>
-                <button className='form--btn'>Get a new meme image</button>
+                <input className='form--btn' type='submit' value='Update text'></input>
             </form>
             
         </main>
