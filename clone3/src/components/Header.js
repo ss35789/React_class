3a0f1eefcc2941332162328c00ddfe3bca934a17
ss35789 <ss35789@naver.com>
2022-07-05 11:37:38 +0900

    update clone3

---
+++ clone3/src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header>
+            <img className='header--img'src="img/face.png" />
+            <h2 className="header--title">Header component! </h2>
+            <h4 className="header--project">React Course - Project3</h4>
+        </header>
+        
+    )
+}
