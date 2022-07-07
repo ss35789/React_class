f4c5a5c054b894c9e51d9634bd9190f2ea0313ad
ss35789 <ss35789@naver.com>
2022-07-07 14:12:22 +0900

    if box was clicked that change color and boxes.js

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -5,10 +5,40 @@ import { useState } from 'react';
 import Box from'./Box';
 function App() {
   const [squares,setSquares]=useState(boxes)
+  
+  function toggle(id){
+    setSquares(prevSquares =>{
+      return prevSquares.map((square)=>{
+         return square.id===id ? {...square,on: !square.on}:square
+      })
+     }
+    )
 
+    //setSquares(prevSquares =>{
+    //   const newSquares =[]
+    //   for(let i=0;i<prevSquares.length;i++){
+    //     const currentSquare=prevSquares[i]
+
+    //     if(currentSquare.id===id){
+    //         const updatedSquare={
+    //           ...currentSquare,
+    //           on : !currentSquare.on
+    //         }
+    //         newSquares.push(updatedSquare)
+    //     }else{
+    //       newSquares.push(currentSquare)
+    //     }
+    //   }
+    //   return newSquares
+    // })
+  }
   const squareElements=squares.map(square=>{
     
-    return <Box id={square.id} on={square.on} />
+    return <Box 
+    key={square.id}
+    id={square.id} 
+    on={square.on}  
+    toggle={toggle}/>
 
    
 })
--- boxes/src/Box.js
+++ boxes/src/Box.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { useState } from "react"
 export default function Box(props){
-    const [on,setOn]= useState(props.on)
     
     const styles={
-        backgroundColor : on ? '#222222' : 'transparent'
-    }
-    function toggle(){
-        setOn(prevOn =>!prevOn)
+        backgroundColor : props.on ? '#222222' : 'transparent'
     }
     
+    
     return(
-        <div style={styles} className='box' onClick={toggle}></div>
+        <div style={styles} className='box' 
+        onClick={()=>
+            props.toggle(props.id)
+        }></div>
     )
 }
