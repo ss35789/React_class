93d5b86db6dbc56b6514e53acc712e798f1ac396
ss35789 <ss35789@naver.com>
2022-07-21 17:03:19 +0900

    .

---
+++ Xlack-Frontend-main/src/components/Addchannel.tsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { createRoom } from "../features/AddChannelSlice";
+import { SidebarInfo } from "./SidebarOption";
+import { AppDispatch } from "../app/store";
+
+
+function Addchannel({Icon,title,id}:SidebarInfo){
+    const dispatch : AppDispatch=useDispatch()
+
+    const addChannel=()=>{
+        
+        const channelName : string=JSON.stringify(prompt('Please enter the channel name'))
+        
+        if(channelName){
+            // db에 name: channelName 방추가
+            
+            axios.post('https://xlack.kreimben.com/api/channel/?channel_name={channelName}',{
+    
+            })
+            .then(res=>{console.log(res)})
+            .catch(console.log('e'))
+        }
+    }
+
+    return(
+        <div className="addchannel"
+            onClick={addChannel}
+        >
+            {Icon && <Icon fontSize='small' style={{padding : 10}}></Icon>}
+            {Icon ? (<h3>{title}</h3>) : 
+                <div>
+                    <span>#</span>{title}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Addchannel
+
